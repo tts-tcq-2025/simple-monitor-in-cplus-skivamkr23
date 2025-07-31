34bf1a3360833e5ad84f6aa22ef5ec25c064272a
Shivam <shivamkr02909@gmail.com>
2025-07-31 21:03:16 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -10,17 +10,26 @@ bool isTemperatureOk(float temperature) {
   return true;
 }
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if (!isTemperatureOk(temperature)) {
-    return false;
-  } else if (soc < 20 || soc > 80) {
+bool isSocOk(float soc) {
+  if (soc < 20 || soc > 80) {
     cout << "State of Charge out of range!\n";
-    return false;
-  } else if (chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
     return false;
   }
   return true;
+}
+
+bool isChargeRateOk(float chargeRate) {
+  if (chargeRate > 0.8) {
+    cout << "Charge Rate out of range!\n";
+    return false
+  }
+  return true;
+}
+
+bool batteryIsOk(float temperature, float soc, float chargeRate) {
+  return isTemperatureOk(temperature) &&
+         isSocOk(soc) &&
+         isChargeRateOk(chargeRate);
 }
 
 int main() {
